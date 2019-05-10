f542dda0f45b1a49094f7c5dafe84023722ebb08
jimmy <jimmy.pan@hotmail.com>
2019-05-10 14:57:42 +0800

    增加wwwroot目錄 增加json時間格式

--- src/Hoshino.API/Startup.cs
+++ src/Hoshino.API/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Hoshino.API.Extentions;
 using Pan.Code.Middleware;
 using Swashbuckle.AspNetCore.Swagger;
+using Newtonsoft.Json.Serialization;
 
 namespace Hoshino.API
 {
@@ -25,20 +26,29 @@ namespace Hoshino.API
             {
                 DBHelper.SQLHelper.SQLHelperFactory.Instance.ConnectionStringsDic[con] = Configuration.GetConnectionString(con);
             }
-            services.AddMvc(config =>{}).SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddMvc(config => { }).AddJsonOptions(options =>
+            {
+                options.SerializerSettings.ContractResolver = new DefaultContractResolver();//设置时间格式
+                options.SerializerSettings.DateFormatString = "yyyy-MM-dd HH:mm:ss";
+            }).SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
             services.RegisDependency();
-            services.AddSwaggerGen(c =>{c.SwaggerDoc("v1", new Info { Title = "My API", Version = "v1" });});
+            services.AddSwaggerGen(c => { c.SwaggerDoc("v1", new Info { Title = "My API", Version = "v1" }); });
         }
 
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment()){ app.UseDeveloperExceptionPage(); }
-            else{ app.UseHsts(); }
+            if (env.IsDevelopment()) { app.UseDeveloperExceptionPage(); }
+            else { app.UseHsts(); }
             app.UseMiddleware<UserExceptionHandlerMiddleware>();
             app.UseHttpsRedirection();
+
+            DefaultFilesOptions options = new DefaultFilesOptions();
+            options.DefaultFileNames.Add("index.html");    //将index.html改为需要默认起始页的文件名.
+            app.UseDefaultFiles(options);
+
             app.UseStaticFiles();
             app.UseSwagger();
-            app.UseSwaggerUI(c =>{ c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1"); });
+            app.UseSwaggerUI(c => { c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1"); });
             app.UseMvc();
         }
     }
