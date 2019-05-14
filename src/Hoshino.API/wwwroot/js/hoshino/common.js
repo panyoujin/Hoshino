4f242e89485a374cc7cc96ee3e25f211854d9ce6
river.xu <1842125096@qq.com>
2019-05-14 17:07:34 +0800

    新增commonjs代码

---
+++ src/Hoshino.API/wwwroot/js/hoshino/common.js
@@ -0,0 +1,25 @@
+﻿$(function () {
+    // var req = { User_Account: "admin", Password: "202cb962ac59075b964b07152d234b70" };
+    var req = { User_Account: "admin", Password: "202cb962ac59075b964b07152d234b70" };
+    requestUrl("/api/sys_user/Login", function (obj) {
+        console.info(obj);
+    }, JSON.stringify(req));
+});
+
+//var domain = "http://www.hosinowt.com";
+var domain = "http://localhost:60737";
+var requestUrl = function (url, callBack, json, type = 'POST') {
+    var urlFull = domain + url;
+    $.ajax({
+        type: type,
+        url: urlFull,
+        dataType: "json",
+        contentType : 'application/json;charset=UTF-8',
+        data: json,
+        success: callBack,
+        error:function(obj){
+            console.info(obj);
+        }
+        
+    });
+};
