a2e1ce4221f2738c277fb8066aa1ae27e88978b8
river.xu <1842125096@qq.com>
2019-05-14 20:30:50 +0800

    add vo

---
+++ src/Hoshino.Entity/ResponseModel/ResLogin.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hoshino.Entity
+{
+    public class ResLogin
+    {
+        public string token { set; get; }
+    }
+}
---
+++ src/Hoshino.Entity/ResponseModel/ResUpload.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hoshino.Entity
+{
+    public class ResUpload
+    {
+        public string filePath { set; get; }
+        public string fileName { set; get; }
+        public string fileType { set; get; }
+    }
+}
