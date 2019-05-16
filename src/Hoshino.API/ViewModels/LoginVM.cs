b38cd109fb7606abb3ad54d8b27b61ca70a98668
river.xu <1842125096@qq.com>
2019-05-16 09:47:29 +0800

    修改model

---
+++ src/Hoshino.API/ViewModels/LoginVM.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hoshino.API.ViewModels
+{
+    public class LoginVM
+    {
+        public string token { set; get; }
+    }
+}
