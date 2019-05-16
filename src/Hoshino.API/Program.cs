7e24a01d0c0ca30524f74cff80a90d4bb4edeb0e
River <1842125096@qq.com>
2019-05-16 21:36:25 +0800

    修改url端口

--- src/Hoshino.API/Program.cs
+++ src/Hoshino.API/Program.cs
@@ -24,6 +24,7 @@ namespace Hoshino.API
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
+            .UseUrls("http://*:81")
             .ConfigureLogging((logging) =>
             {
                 logging.AddFilter("System", LogLevel.Warning);
