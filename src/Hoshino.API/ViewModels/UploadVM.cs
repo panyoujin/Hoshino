100d8a07226dd26b3078b8dbc2a2199f41948ee7
jimmy <jimmy.pan@hotmail.com>
2019-05-16 10:20:41 +0800

    增加UploadVM

---
+++ src/Hoshino.API/ViewModels/UploadVM.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hoshino.API.ViewModels
+{
+    public class UploadVM
+    {
+        public string filePath { set; get; }
+        public string fileName { set; get; }
+        public string fileType { set; get; }
+    }
+}
