33ed7943ebe66ba0038b36f9dc458ce5cb3dca86
River <1842125096@qq.com>
2019-05-20 22:50:15 +0800

    产品新增编辑页面静态

--- src/Hoshino.API/wwwroot/js/hoshino/common.js
+++ src/Hoshino.API/wwwroot/js/hoshino/common.js
@@ -23,7 +23,7 @@
 });
 
 //var domain = "http://www.hosinowt.com";
-var domain = "";
+var domain = "http://localhost:11111";
 var requestUrl = function (url, callBack, json, type = 'POST') {
     var urlFull = domain + url;
     $.ajax({
