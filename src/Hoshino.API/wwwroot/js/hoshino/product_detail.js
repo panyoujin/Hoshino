ece757358cb1574f4b5eeb7d7e3895f67fcd40a7
river.xu <1842125096@qq.com>
2019-05-22 22:06:07 +0800

    修改产品详情

---
+++ src/Hoshino.API/wwwroot/js/hoshino/product_detail.js
@@ -0,0 +1,16 @@
+$(function () {
+    $("#imgPic1,#imgPic2,#imgPic3").click(function(){
+        
+        //  alert($(this).attr("id"));
+    });
+
+    $("#download1").click(function(){
+
+        //  console.info($(".image-crop > img").cropper("getDataURL"));
+
+        // var o = $(".image-crop > img");
+        // alert(o.cropper("getDataURL"));
+        // window.open(o.cropper("getDataURL"))
+    });
+});
+
