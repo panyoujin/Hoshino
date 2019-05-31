409e0b1dd4b659650334907826399edd6124face
river.xu <1842125096@qq.com>
2019-05-31 16:37:36 +0800

    修改后台首页

---
+++ src/Hoshino.API/wwwroot/js/hoshino/echarts.js
@@ -0,0 +1,105 @@
+$(function () {
+    loadAppointment();
+    loadContact();
+});
+
+
+var loadAppointment = function () {
+    // 基于准备好的dom，初始化echarts图表
+    var echartsAppointment = echarts.init(document.getElementById('echarts-appointment'));
+    optionAppointment = {
+        tooltip: {
+            trigger: 'axis'
+        },
+        legend: {
+            data: ['咨询量', '已处理']
+        },
+        toolbox: {
+            show: true,
+            feature: {
+                mark: { show: true },
+                dataView: { show: true, readOnly: false },
+                magicType: { show: true, type: ['line', 'bar'] },
+                restore: { show: true },
+                saveAsImage: { show: true }
+            }
+        },
+        calculable: true,
+        xAxis: [
+            {
+                type: 'category',
+                data: ['1月', '2月', '3月', '4月', '5月', '6月', '7月', '8月', '9月', '10月', '11月', '12月']
+            }
+        ],
+        yAxis: [
+            {
+                type: 'value'
+            }
+        ],
+        series: [
+            {
+                name: '咨询量',
+                type: 'bar',
+                data: [2.0, 4.9, 7.0, 23.2, 25.6, 76.7, 135.6, 162.2, 32.6, 20.0, 6.4, 3.3]
+            },
+            {
+                name: '已处理',
+                type: 'bar',
+                data: [2.6, 5.9, 9.0, 26.4, 28.7, 70.7, 175.6, 182.2, 48.7, 18.8, 6.0, 2.3]
+            }
+        ]
+    };
+
+    // 为echarts对象加载数据 
+    echartsAppointment.setOption(optionAppointment);
+}
+
+var loadContact = function () {
+    // 基于准备好的dom，初始化echarts图表
+    var echartsContact = echarts.init(document.getElementById('echarts-contact'));
+    optionContact = {
+        tooltip: {
+            trigger: 'axis'
+        },
+        legend: {
+            data: ['咨询量', '已处理']
+        },
+        toolbox: {
+            show: true,
+            feature: {
+                mark: { show: true },
+                dataView: { show: true, readOnly: false },
+                magicType: { show: true, type: ['line', 'bar'] },
+                restore: { show: true },
+                saveAsImage: { show: true }
+            }
+        },
+        calculable: true,
+        xAxis: [
+            {
+                type: 'category',
+                data: ['1月', '2月', '3月', '4月', '5月', '6月', '7月', '8月', '9月', '10月', '11月', '12月']
+            }
+        ],
+        yAxis: [
+            {
+                type: 'value'
+            }
+        ],
+        series: [
+            {
+                name: '咨询量',
+                type: 'bar',
+                data: [2.0, 4.9, 7.0, 23.2, 25.6, 76.7, 135.6, 162.2, 32.6, 20.0, 6.4, 3.3]
+            },
+            {
+                name: '已处理',
+                type: 'bar',
+                data: [2.6, 5.9, 9.0, 26.4, 28.7, 70.7, 175.6, 182.2, 48.7, 18.8, 6.0, 2.3]
+            }
+        ]
+    };
+
+    // 为echarts对象加载数据 
+    echartsContact.setOption(optionContact);
+}
